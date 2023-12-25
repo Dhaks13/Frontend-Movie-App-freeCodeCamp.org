2a86cc5371df3f0f82b83834894a847b22823e12
Dhakshin A V <avdhakshin1354@gmail.com>
2023-12-25 21:52:16 +0530

    created - script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7f9bbe9b5ed99f038c60db0813faa5e4&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1380';
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=7f9bbe9b5ed99f038c60db0813faa5e4&query=";
+
