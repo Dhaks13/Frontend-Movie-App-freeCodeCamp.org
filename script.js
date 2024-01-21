d860628ffd33415be9b3f9a5e6169632c728daae
Dhakshin A V <avdhakshin1354@gmail.com>
2024-01-21 09:43:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const APIKEY = "7f9bbe9b5ed99f038c60db0813faa5e4";
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${APIKEY}&page=1';
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7f9bbe9b5ed99f038c60db0813faa5e4&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2';
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=${APIKEY}&query=";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=7f9bbe9b5ed99f038c60db0813faa5e4&query=";
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
